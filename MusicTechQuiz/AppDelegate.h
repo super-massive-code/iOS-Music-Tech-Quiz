192c2121cf8732209360c446e69777686e699730
Carl Taylor <carltaylor43uk@gmail.com>
2015-10-25 10:00:01 +0000

    Initial Commit

---
+++ MusicTechQuiz/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MusicTechQuiz
+//
+//  Created by Carl  on 25/10/2015.
+//  Copyright © 2015 Carl Taylor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MusicTechQuiz/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MusicTechQuiz
+//
+//  Created by Carl  on 25/10/2015.
+//  Copyright © 2015 Carl Taylor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
