e99d7ca147a99fa066ad5264ee401203472407e4
Carl Taylor <carltaylor43uk@gmail.com>
2016-01-06 16:17:11 +0000

    ADDED: circular UIViews

--- MusicTechQuiz/IntroViewController.m
+++ MusicTechQuiz/IntroViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "IntroViewController.h"
+#import "GlobalConstants.h"
+#import <QuartzCore/QuartzCore.h>
 
 @interface IntroViewController ()
 
@@ -44,24 +46,36 @@ NSString * const segueQuestionAnswerVC = @"segueQuestionAnswerVC";
 -(void)setUpUi
 {
     [super setUpUi];
+    
+    self.messageLabel.font = [FontConstants secondaryTitleFont];
+    self.messageLabel.textColor = [ColourConstants primaryTextColour];
+    
+    self.view.backgroundColor = [UIColor blackColor];
 }
 
 #pragma mark -
-#pragma mark Intro
+#pragma mark GenerateIntro
 
 -(void)startIntroProcess
 {
     NSMutableArray *messages = [self createMessages];
-    [self animateMessages:messages WithCallBack:^{
-//        [self startIntroProcess];
-        [self startGame];
+    
+    int totalAnimationDurationSeconds = 5;
+    float individualAnimationTime = totalAnimationDurationSeconds/messages.count;
+    
+    UIView *circles = [self createCircles];
+    [self.view insertSubview:circles atIndex:0];
+    
+    [self animateMessages:messages forDuration:individualAnimationTime WithCallBack:^{
+        [self startIntroProcess];
+//        [self startGame];
     }];
 }
 
 -(NSMutableArray*)createMessages
 {
     NSMutableArray *messages = [[NSMutableArray alloc]init];
-    [messages addObject:@"Get\nReady"];
+    [messages addObject:@"Get\nReady!"];
     [messages addObject:@"3"];
     [messages addObject:@"2"];
     [messages addObject:@"1"];
@@ -70,7 +84,7 @@ NSString * const segueQuestionAnswerVC = @"segueQuestionAnswerVC";
     return messages;
 }
 
--(void)animateMessages:(NSMutableArray*)messages WithCallBack:(void(^)(void))callBack
+-(void)animateMessages:(NSMutableArray*)messages forDuration:(float)duration WithCallBack:(void(^)(void))callBack
 {
     NSString *messageString = [messages objectAtIndex:0];
     self.messageLabel.text = messageString;
@@ -79,16 +93,46 @@ NSString * const segueQuestionAnswerVC = @"segueQuestionAnswerVC";
     CGAffineTransform transformGrow   = CGAffineTransformMakeScale(4.0f, 4.0f);
     CGAffineTransform transformShrink = CGAffineTransformMakeScale(1.0f, 1.0f);
     
-    [UIView animateWithDuration:1 delay:0 options:UIViewAnimationOptionCurveEaseOut animations:^{
+    [UIView animateWithDuration:duration delay:0 options:UIViewAnimationOptionCurveEaseOut animations:^{
         self.messageLabel.transform = transformGrow;
     } completion:^(BOOL finished) {
         self.messageLabel.transform = transformShrink;
         if (messages.count > 0) {
-            [self animateMessages:messages WithCallBack:callBack];
+            [self animateMessages:messages forDuration:duration WithCallBack:callBack];
         } else {
             callBack();
         }
     }];
+}
+
+#pragma mark -
+#pragma mark CreateCircles
+
+-(UIView*)createCircles
+{
+    float diamieter = self.view.bounds.size.width / 2;
+ 
+    UIView *circleView = [[UIView alloc]init];
+    circleView.center = self.view.center;
+    circleView.backgroundColor = [ColourConstants secondaryBackgroundColour];
+    [self roundView:circleView toDiameter:diamieter];
+
+    UIView *innerCircleView = [[UIView alloc]init];
+    innerCircleView.backgroundColor = [ColourConstants primaryBackgroundColour];
+    [self roundView:innerCircleView toDiameter:diamieter - 20];
+    innerCircleView.center = CGPointMake(circleView.bounds.size.width / 2, circleView.bounds.size.width / 2);
+
+    [circleView addSubview:innerCircleView];
+    return circleView;
+}
+
+-(void)roundView:(UIView *)view toDiameter:(float)newSize;
+{
+    CGPoint saveCenter = view.center;
+    CGRect newFrame = CGRectMake(view.frame.origin.x, view.frame.origin.y, newSize, newSize);
+    view.frame = newFrame;
+    view.layer.cornerRadius = newSize / 2.0;
+    view.center = saveCenter;
 }
 
 #pragma mark -
