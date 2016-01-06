7f10cfb45379c8577093aef49f5e625d6223f973
Carl Taylor <carltaylor43uk@gmail.com>
2016-01-06 14:39:33 +0000

    ADDED: recursive animation of label with messages

--- MusicTechQuiz/IntroViewController.m
+++ MusicTechQuiz/IntroViewController.m
@@ -10,9 +10,11 @@
 
 @interface IntroViewController ()
 
+@property (weak, nonatomic) IBOutlet UILabel *messageLabel;
+
 @end
 
-const NSString *segueQuestionAnswerVC = @"segueQuestionAnswerVC";
+NSString * const segueQuestionAnswerVC = @"segueQuestionAnswerVC";
 
 @implementation IntroViewController
 
@@ -22,7 +24,12 @@ const NSString *segueQuestionAnswerVC = @"segueQuestionAnswerVC";
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-  
+}
+
+-(void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:YES];
+    [self startIntroProcess];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -30,5 +37,57 @@ const NSString *segueQuestionAnswerVC = @"segueQuestionAnswerVC";
     // Dispose of any resources that can be recreated.
 }
 
+
+#pragma mark -
+#pragma mark Intro
+
+-(void)startIntroProcess
+{
+    NSMutableArray *messages = [self createMessages];
+    [self animateMessages:messages WithCallBack:^{
+        [self startGame];
+    }];
+}
+
+-(NSMutableArray*)createMessages
+{
+    NSMutableArray *messages = [[NSMutableArray alloc]init];
+    [messages addObject:@"Get\nReady!"];
+    [messages addObject:@"3"];
+    [messages addObject:@"2"];
+    [messages addObject:@"1"];
+    [messages addObject:@"Go!"];
+    
+    return messages;
+}
+
+-(void)animateMessages:(NSMutableArray*)messages WithCallBack:(void(^)(void))callBack
+{
+    NSString *messageString = [messages objectAtIndex:0];
+    self.messageLabel.text = messageString;
+    [messages removeObjectAtIndex:0];
+    
+    CGAffineTransform transformGrow   = CGAffineTransformMakeScale(4.0f, 4.0f);
+    CGAffineTransform transformShrink = CGAffineTransformMakeScale(1.0f, 1.0f);
+    
+    [UIView animateWithDuration:1 delay:0 options:UIViewAnimationOptionCurveEaseOut animations:^{
+        self.messageLabel.transform = transformGrow;
+    } completion:^(BOOL finished) {
+        self.messageLabel.transform = transformShrink;
+        if (messages.count > 0) {
+            [self animateMessages:messages WithCallBack:callBack];
+        } else {
+            callBack();
+        }
+    }];
+}
+
+#pragma mark -
+#pragma mark StartGame
+
+-(void)startGame
+{
+    [self performSegueWithIdentifier:segueQuestionAnswerVC sender:self];
+}
 
 @end
--- MusicTechQuiz/QuestionAnswerViewControllerNew.h
+++ MusicTechQuiz/QuestionAnswerViewControllerNew.h
@@ -11,4 +11,6 @@
 
 @interface QuestionAnswerViewControllerNew : BaseViewController
 
+
+
 @end
