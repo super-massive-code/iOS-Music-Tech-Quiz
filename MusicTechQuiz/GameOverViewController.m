fde6707d0682749127e219042f2c882c815f0c34
Carl Taylor <carltaylor43uk@gmail.com>
2016-01-07 09:00:46 +0000

    ADDED: styling and setting of score to gameOverViewController

--- MusicTechQuiz/GameOverViewController.m
+++ MusicTechQuiz/GameOverViewController.m
@@ -7,11 +7,13 @@
 //
 
 #import "GameOverViewController.h"
+#import "GlobalConstants.h"
 
 @interface GameOverViewController ()
 
-@property (weak, nonatomic) IBOutlet UIButton *homeButton;
+@property (strong, nonatomic) IBOutletCollection(UIButton) NSArray *buttons;
 
+@property (weak, nonatomic) IBOutlet UILabel *scoreLabel;
 @end
 
 @implementation GameOverViewController
@@ -22,12 +24,43 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    [self setUpUi];
+    [self setScore];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark -
+#pragma mark SetUp
+
+-(void)setUpUi
+{
+    [super setUpUi];
+    
+    UIFont *buttonFont  = [FontConstants secondaryTitleFont];
+    UIFont *pointsFont = [FontConstants titleFont];
+
+    UIColor *textColour = [ColourConstants primaryTextColour];
+    UIColor *backgroundColour = [ColourConstants primaryBackgroundColour];
+
+    self.view.backgroundColor = backgroundColour;
+    
+    for (UIButton *button in self.buttons) {
+        button.layer.borderColor = textColour.CGColor;
+        button.layer.borderWidth = 2.0f;
+        button.layer.cornerRadius = 10.0f;
+        [button setTitleColor:textColour forState:UIControlStateNormal];
+        [button.titleLabel setFont:buttonFont];
+    }
+    
+    self.scoreLabel.textColor = textColour;
+    self.scoreLabel.font = pointsFont;
+}
+
+#pragma mark -
+#pragma mark Score
+
+-(void)setScore
+{
+    self.scoreLabel.text = [NSString stringWithFormat:@"Your Score\n%@", self.userScore];
 }
 
 #pragma mark -
